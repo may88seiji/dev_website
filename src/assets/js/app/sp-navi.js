0afa5d6de09f7c40fb6233b6cedaed3957c21d23
may88seiji <may88seiji@gmail.com>
2017-10-13 15:01:25 +0900

    adjust sp scroll

--- src/assets/js/app/sp-navi.js
+++ src/assets/js/app/sp-navi.js
@@ -1,5 +1,11 @@
 export function toggleNavi(){
   $(".js-navi").on('click',function(){
     $(this).toggleClass("is-open");
+    
+    if($('.js-navi').hasClass("is-open")){
+      $('.l-main').addClass("is-fix");
+    }else{
+      $('.l-main').removeClass("is-fix");
+    }
   })
 }
