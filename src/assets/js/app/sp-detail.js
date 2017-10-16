9b0a3ef9e65de1d9d735bf4d42abea77f462aee9
may88seiji <may88seiji@gmail.com>
2017-10-16 10:59:39 +0900

    adjust detail.js

--- src/assets/js/app/sp-detail.js
+++ src/assets/js/app/sp-detail.js
@@ -3,11 +3,12 @@ export function init(){
   let w = $(".wysiwyg p").width();
   let $header = $detail.prev('.l-header');
   let $pager = $detail.find('.l-pager');
-  let hide = {'opacity' :'0', 'pointer-events': 'none'};
-  let show = {'opacity' :'1', 'pointer-events': 'inherit', 'transition': '1000ms'};
+  let init = {'opacity' :'0', 'pointer-events': 'none'};
+  let hide = {'opacity' :'0', 'pointer-events': 'none','transition': '1000ms'}};
+  let show = {'opacity' :'1', 'pointer-events': 'inherit', 'transition': '1500ms'};
   
-  $header.css(hide);
-  $pager.css(hide);
+  $header.css(init);
+  $pager.css(init);
   
   $('.js-scrollX').on('scroll',function(){
     let scrollX = $(this).scrollLeft();
