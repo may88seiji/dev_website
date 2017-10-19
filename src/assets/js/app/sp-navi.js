15542d946019f4c87c0ea4236c88720ec2dc19c0
may88seiji <may88seiji@gmail.com>
2017-10-19 19:03:18 +0900

    adjust is-fix

--- src/assets/js/app/sp-navi.js
+++ src/assets/js/app/sp-navi.js
@@ -1,11 +1,18 @@
 export function toggleNavi(){
   $(".js-navi").on('click',function(){
+    let st = $(window).scrollTop();
+    
     $(this).toggleClass("is-open");
     
     if($('.js-navi').hasClass("is-open")){
-      $('.l-main').addClass("is-fix");
+      $('body').addClass("is-fix");
+//      $('.l-main').css({'top': -st});
+//      window.scrollTop(0,st);
+      $("html,body").animate({scrollTop:st});
     }else{
-      $('.l-main').removeClass("is-fix");
+      $('body').removeClass("is-fix");
+//      $('.l-main').css({'top': 0});
+//      window.scrollTop(0,st);
     }
   })
 }
