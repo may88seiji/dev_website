7ae7597982ccf6ffac631be1be476e554172f2fe
may88seiji <may88seiji@gmail.com>
2017-10-21 19:27:57 +0900

    adjust pager

--- www/html/assets/themes/takedasei/singles/works.php
+++ www/html/assets/themes/takedasei/singles/works.php
@@ -1,4 +1,10 @@
-<?php get_header(); ?>
+<?php
+$next_post = get_next_post();
+$prev_poxt = get_previous_post();
+
+get_header(); 
+?>
+
 <div class="l-container detail js-detail">
     <div class="l-content">
       <div class="l-currentPage">works</div>
@@ -23,15 +29,19 @@
               </p>
             </div>
           </div>
-
         </div>
       </div>
 
       <div class="l-pager">
         <ul class="pager">
           <li><a href="<?php echo home_url('');?>/works"><i class="icon-dots"></i></a></li>
-          <li class="sp-hide"><a href=""><i class="icon-arrow_left"></i></a></li>
-          <li class="sp-hide"><a href=""><i class="icon-arrow_right"></i></a></li>
+          <?php if (!empty( $next_post )): ?>
+          <li class="sp-hide"><a href="<? echo get_permalink( $next_post->ID );?>"><i class="icon-arrow_left"></i></a></li>
+          <?php endif;
+          if (!empty( $prev_poxt  )): ?>
+          <li class="sp-hide"><a href="<?php echo get_permalink( $prev_poxt->ID );?>"><i class="icon-arrow_right"></i></a></li>
+          <?php endif; ?>
+         
         </ul>
       </div>
 
